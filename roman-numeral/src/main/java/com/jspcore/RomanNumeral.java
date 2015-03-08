5a37cac03ec33413f4740e8f4337cbc64854e295
MuSTa1nE <mustainer@gmail.com>
2015-03-08 15:22:17 +0000

    Adding exception cases

--- roman-numeral/src/main/java/com/jspcore/RomanNumeral.java
+++ roman-numeral/src/main/java/com/jspcore/RomanNumeral.java
@@ -10,19 +10,19 @@ public class RomanNumeral {
     public static String romanFrom(int number) {
         if (number == 5) return "V";
         return IntStream.iterate(number, i -> i - RomanToDecimal.applyTo(i).decimal)
-                .limit(number)
+                .limit(RomanToDecimal.values().length)
                 .filter(i -> i > 0)
                 .mapToObj(i -> RomanToDecimal.applyTo(i).name())
                 .collect(Collectors.joining());
     }
 
     enum RomanToDecimal {
-        M(1000),
-        D(500),
-        C(100),
-        L(50),
-        X(10),
-        V(5),
+        M(1000), CM(900),
+        D(500), CD(400),
+        C(100), XC(90),
+        L(50), XL(40),
+        X(10), IX(9),
+        V(5), IV(4),
         I(1);
 
         int decimal;
--- roman-numeral/src/test/java/com/jspcore/RomanNumeralShould.java
+++ roman-numeral/src/test/java/com/jspcore/RomanNumeralShould.java
@@ -12,14 +12,19 @@ public class RomanNumeralShould {
         assertThat(RomanNumeral.romanFrom(1), is("I"));
         assertThat(RomanNumeral.romanFrom(2), is("II"));
         assertThat(RomanNumeral.romanFrom(3), is("III"));
+        assertThat(RomanNumeral.romanFrom(4), is("IV"));
         assertThat(RomanNumeral.romanFrom(5), is("V"));
         assertThat(RomanNumeral.romanFrom(6), is("VI"));
         assertThat(RomanNumeral.romanFrom(8), is("VIII"));
+        assertThat(RomanNumeral.romanFrom(9), is("IX"));
         assertThat(RomanNumeral.romanFrom(10), is("X"));
         assertThat(RomanNumeral.romanFrom(16), is("XVI"));
         assertThat(RomanNumeral.romanFrom(51), is("LI"));
+        assertThat(RomanNumeral.romanFrom(92), is("XCII"));
         assertThat(RomanNumeral.romanFrom(103), is("CIII"));
+        assertThat(RomanNumeral.romanFrom(407), is("CDVII"));
         assertThat(RomanNumeral.romanFrom(507), is("DVII"));
+        assertThat(RomanNumeral.romanFrom(909), is("CMIX"));
         assertThat(RomanNumeral.romanFrom(3387), is("MMMCCCLXXXVII"));
     }
 }
