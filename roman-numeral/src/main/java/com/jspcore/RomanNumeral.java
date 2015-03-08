87515ff3dc695f32271912dba73463c708ec0d4c
MuSTa1nE <mustainer@gmail.com>
2015-03-08 15:17:56 +0000

    Refactoring  with an Enum

--- roman-numeral/src/main/java/com/jspcore/RomanNumeral.java
+++ roman-numeral/src/main/java/com/jspcore/RomanNumeral.java
@@ -1,5 +1,7 @@
 package com.jspcore;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -7,10 +9,29 @@ public class RomanNumeral {
 
     public static String romanFrom(int number) {
         if (number == 5) return "V";
-        return IntStream.iterate(number, i -> i - (i >= 10 ? 10 : i >= 5 ? 5 : 1))
+        return IntStream.iterate(number, i -> i - RomanToDecimal.applyTo(i).decimal)
                 .limit(number)
                 .filter(i -> i > 0)
-                .mapToObj(i -> i >= 10 ? "X" : i >= 5 ? "V" : "I")
+                .mapToObj(i -> RomanToDecimal.applyTo(i).name())
                 .collect(Collectors.joining());
+    }
+
+    enum RomanToDecimal {
+        X(10),
+        V(5),
+        I(1);
+
+        int decimal;
+
+        RomanToDecimal(int decimal) {
+            this.decimal = decimal;
+        }
+
+        public static RomanToDecimal applyTo(int number) {
+            return Collections.unmodifiableCollection(Arrays.asList(RomanToDecimal.values())).stream()
+                    .filter(roman -> number >= roman.decimal)
+                    .findFirst()
+                    .orElse(I);
+        }
     }
 }
